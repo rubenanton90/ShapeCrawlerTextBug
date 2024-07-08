a593b4bc468d89a541a7a472d97b01814a9a4fd1
Rubén Antón García <rubenanton90@hotmail.com>
2024-07-08 11:34:53 +0200

    Add project files.

---
+++ ShapeCrawlerTextBug/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DocumentFormat.OpenXml.Bibliography;
+using ShapeCrawler;
+
+
+var pres = new Presentation("./samplepptx_es.pptx");
+//var textFrames = pres.Slides.SelectMany(e => e.TextFrames());
+
+foreach (var slide in pres.Slides)
+{
+
+    foreach (ITextFrame item in slide.TextFrames())
+    {
+        string t = item.Text;
+        item.Text = ReverseText(t);
+    }
+}
+
+pres.SaveAs($"./samplepptx_es_{DateTime.Now.ToString("_dd_MM_yyyy_HHmmssfff")}.pptx");
+
+static string ReverseText(string s)
+{
+    char[] charArray = s.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
